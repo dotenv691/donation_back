42926a9cecb29e1b06f8bbb31f4ba8b6d3c0a43f
Adiya Ganbat <yagamipassion03@gmail.com>
2023-04-25 13:03:07 +0800

    .

--- app/Http/Controllers/Api/CustomResponseController.php
+++ app/Http/Controllers/Api/CustomResponseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Donate;
 
 class CustomResponseController extends Controller
 {
@@ -31,31 +32,27 @@ class CustomResponseController extends Controller
         // return redirect()->to('http://localhost:3000/donate-now?status=error');
     }
 
-    public function paymentapprove(Request $request) {
+    public function paymentapprove(Request $request, Donate $donate) {
+
+        // parse xml
         $xml_data = $request->xmlmsg;
         $xmlmsg = str_replace("\\\"","\"",$xml_data);
         $xml = simplexml_load_string($xmlmsg, "SimpleXMLElement", LIBXML_NOCDATA);
         $json = json_encode($xml);
         $array = json_decode($json,TRUE);
-        echo 'OrderID: '.$array['OrderID']."<br>";
-        echo 'Version: '.$array['Version']."<br>";
-        echo 'TransactionType: '.$array['TransactionType']."<br>";
-        echo 'PAN: '.$array['PAN']."<br>";
-        echo 'PurchaseAmount: '.$array['PurchaseAmount']."<br>";
-        echo 'Currency: '.$array['Currency']."<br>";
-        echo 'TranDateTime: '.$array['TranDateTime']."<br>";
-        echo 'ResponseCode: '.$array['ResponseCode']."<br>";
-        echo 'OrderStatus: '.$array['OrderStatus']."<br>";
-        echo 'ApprovalCode: '.$array['ApprovalCode']."<br>";
-        echo 'MerchantTranID: '.$array['MerchantTranID']."<br>";
-        echo 'OrderDescription: '.$array['OrderDescription']."<br>";
-        echo 'ApprovalCodeScr: '.$array['ApprovalCodeScr']."<br>";
-        echo 'PurchaseAmountScr: '.$array['PurchaseAmountScr']."<br>";
-        echo 'CurrencyScr: '.$array['CurrencyScr']."<br>";
-        echo 'OrderStatusScr: '.$array['OrderStatusScr']."<br>";
-        // echo 'ThreeDSVerificaion: '.$array['ThreeDSVerificaion']."<br>";
-        echo 'ShopOrderId: '.$array['ShopOrderId']."<br>";
-        echo 'SignatureValue: '.$array['SignatureValue']."<br>";
+
+        // update to server
+        $donate->where('id', $array['ShopOrderId']);
+        $donate->type = $array['MerchantTranID'];
+        $donate->value = $array['PurchaseAmountScr'];
+        $donate->card_number = $array['PAN'];
+        $donate->exp = $array['ApprovalCodeScr'];
+        $donate->verf = $array['OrderStatus'];
+        $donate->name = $array['CurrencyScr'];
+        $donate->update();
+        $info = $donate->first();
+
+        echo '<pre>' . var_export($info, true) . '</pre>';
     }
     public function paymentreject(Request $request) {
         echo 'reject';
