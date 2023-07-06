4f0fa1bfdf61b8a10ff003c1071e46b23343349d
dotenv691 <110764961+dotenv691@users.noreply.github.com>
2023-07-06 20:34:10 +0800

    .

--- app/Http/Controllers/Api/CustomResponseController.php
+++ app/Http/Controllers/Api/CustomResponseController.php
@@ -84,7 +84,7 @@ class CustomResponseController extends Controller
         $index = 0;
         foreach ( $responsearr as $ind => $item ) {
             if($index == 0) $repo .= '{';
-            $repo .= str_replace($replace_string, '', $item).',';
+            $repo .= $item.',';
             $index +=1;
         }
         $repo .= '}';
@@ -175,7 +175,7 @@ class CustomResponseController extends Controller
         $index = 0;
         foreach ( $responsearr as $ind => $item ) {
             if($index == 0) $repo .= '{';
-            $repo .= str_replace($replace_string, '', $item).',';
+                $repo .= $item.',';
             $index +=1;
         }
         $repo .= '}';
