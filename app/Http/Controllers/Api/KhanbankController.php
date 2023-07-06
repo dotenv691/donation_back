03d49294dcfadd316e0ee6b7e53b7f8cbef8e32e
dotenv691 <110764961+dotenv691@users.noreply.github.com>
2023-07-06 23:27:42 +0800

    .

--- app/Http/Controllers/Api/KhanbankController.php
+++ app/Http/Controllers/Api/KhanbankController.php
@@ -60,7 +60,6 @@ class KhanbankController extends Controller
 
         $responsearr = explode(',', $response);
         $replace_string = array('"formUrl":"','"}');
-        $follow_url = str_replace($replace_string, '',$responsearr[2]);
 
         $replace_string = array('"orderId":"','"');
         $cookie_value = str_replace($replace_string, '',$responsearr[1]);
